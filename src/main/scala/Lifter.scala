ea14c4da4f0269b625c8f3be41e0848dbba94bbc
VladimirNik <vladimir.nikolaev9@gmail.com>
2013-10-15 23:07:02 +0400

    testing code added

--- src/main/scala/Lifter.scala
+++ src/main/scala/Lifter.scala
@@ -12,7 +12,26 @@ object Lifter {
     def m(c: Context): c.Expr[Unit] = {
       import c.universe._
 
-      println(PrettyPrinter.show(c.resetAllAttrs(c.enclosingUnit.body)))
+      val testTree: Tree = c.resetAllAttrs(c.enclosingUnit.body)
+
+      val testAny: Any = testTree
+      println("testAny.isInstanceOf[PrettyPrinter.global.Tree]: " + testAny.isInstanceOf[PrettyPrinter.global.Tree])
+      testAny match {
+        case gt: PrettyPrinter.global.Tree => println("testAny - global.Tree - Match")
+        case _ => println("testAny - global.Tree - Doesn't match")
+      }
+
+      val testGlobal: PrettyPrinter.global.Tree = testAny.asInstanceOf[PrettyPrinter.global.Tree]
+      println("testGlobal.isInstanceOf[PrettyPrinter.global.Tree]: " + testTree.isInstanceOf[PrettyPrinter.global.Tree])
+      testGlobal match {
+        case gt: PrettyPrinter.global.Tree => println("testGlobal - global.Tree - Match")
+        case _ => println("testGlobal - global.Tree - Doesn't match")
+      }
+
+      println("=========")
+      println()
+
+      println(PrettyPrinter.show(testTree))
 
       c.literalUnit
     }
